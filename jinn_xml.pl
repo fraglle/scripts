dc12341935837875ccdc21b1888c83568e39275f
fraglle <_under_@mail.ru>
2015-08-20 22:22:40 +0300

    22:22

--- jinn_xml.pl
+++ jinn_xml.pl
@@ -1,116 +1,74 @@
 #!/usr/bin/perl
 #
-# установка модуля sudo cpan MIME::Lite::TT::HTML
-#
-#
+
 
 use strict;
 use warnings;
-use MIME::Lite::TT::HTML;
+use MIME::Lite;
+use Data::Dumper;
+use XML::Simple;
+use Encode;
 
-my $conf_file = 'jinn.conf';
-my $mail_file = 'mail.list';
-my $body_conf_file = 'body.conf';
-my %config = ();
-my $config_line;
-my %options;
-my %params;
-#$params{recipient} = {''};
 
-if(!open(CONFIG, "$conf_file"))
+my $conf_file = 'jinn.xml';
+my $mail_list = 'mail.list';
+my $mail_xml = 'mail.xml';
+
+
+unless (-e $conf_file) { die "Error open config file : $conf_file $!";}
+unless (-e $mail_list) { die "Error open config file : $mail_list $!";}
+unless (-e $mail_xml) { die "Error open config file : $mail_xml $!";}
+
+my $config = XMLin("$conf_file");
+my $email = XMLin("$mail_xml");
+open (MAILBASE, $mail_list) or die "Can't open file $mail_list $!";
+
+#print Dumper $config;
+print Dumper $email;
+
+Encode::_utf8_off($email->{subject});
+
+while (<MAILBASE>)
 {
-    print "Error: Config file not found : $conf_file";
-    exit(0);
-}
+    my $msg = MIME::Lite->new(
+        From    => "$email->{from}",
+        To      => "$_",
+        Subject => "$email->{subject}",
+        Type    => 'multipart/mixed',
+    );
 
-while(<CONFIG>)
-{
-    $config_line = $_;
-    chop($config_line);
-    s/#.*//;                # no comments
-    s/^\s+//;               # no leading white
-    s/\s+$//;               # no trailing white
-    next unless length;     # anything left?
-    my ($var, $value) = split(/\s*=\s*/, $_, 2);
-    $config{$var} = $value;
-}
+    $msg->attach( Type => 'text/html; charset=UTF-8',
+                  Data => "$email->{emailbody}"
+    );
 
-if(defined($ARGV[0]) and  $ARGV[0] eq '-p')
-{
-    foreach my $conf_key(keys %config)
+    if(!ref($email->{emailattachments}))
     {
-        print "$conf_key = $config{$conf_key}\n";
+        print "Scalar\n";
+        $msg->attach( Type        => 'image/jpg',
+                      Path        => "$email->{emailattachments}",
+                      Filename    => "$email->{emailattachments}",
+                      Disposition => 'attachment' );
     }
-}
-
-if(!open(BODYCONF,"$body_conf_file"))
-{
-    print "Error: Mailbase file not found: $mail_file";
-    exit(0);
-}
-while(<BODYCONF>)
-{
-    chomp;
-    s/#.*//;                # no comments
-    s/^\s+//;               # no leading white
-    s/\s+$//;               # no trailing white
-    next unless length;
-    $params{$_} = "$_\.gif";
-}
-
-if(!open(MAILBASE,"$mail_file"))
-{
-    print "Error: Mailbase file not found: $mail_file";
-    exit(0);
-}
-
-while(<MAILBASE>)
-{
-    chomp;
-    s/#.*//;                # no comments
-    s/^\s+//;               # no leading white
-    s/\s+$//;               # no trailing white
-    next unless length;
-    my $recipient = $_;
-    $params{recipient} = $recipient;
-
-    my $msg = MIME::Lite::TT::HTML->new(
-       From        =>  'imis@ptk.ru',
-       To          =>  "$params{recipient}",
-       Subject     =>  'Test mail',
-       Template    =>  {
-                     html    =>  'body.html',
-                     text    =>  'body.txt'
-
-                   },
-       Charset     => 'utf8',
-       Timezone  => 'UTC',
-       Type      => 'multipart/mixed',
-       Encoding  => 'base64',
-       TmplOptions =>  \%options,
-       TmplParams  =>  \%params,
-       );
-
-    $msg->attr("content-type"  => "multipart/mixed");
-
-    for my $key(keys %params)
+    else
     {
-        print "$params{$key}\n";
-        if(index($params{$key},"\@") != -1)
+        print "Hash\n";
+        for my $key (keys $email->{emailattachments} )
         {
-            #print "Email adress : $params{$key}\n";
-            next;
+            $msg->attach( Type        => 'image/jpg',
+                          Path        => "$email->{emailattachments}[$key]",
+                          Filename    => "$email->{emailattachments}[$key]",
+                          Disposition => 'attachment' );
         }
-        $msg->attach(  Type        =>  'image/gif',
-            Path        =>  "$params{$key}",
-            Filename    =>  "$params{$key}",
-            Disposition =>  'attachment'
-        );
     }
 
-
-    $msg->send('smtp', '10.10.101.29', Timeout => 60 );;
+    $msg->send('smtp',
+        $config->{server_smtp},
+        #SSL=>1,
+        #AuthUser=>"$user",
+        #AuthPass=>"$pass",
+        Debug=>0);
 }
+close(MAILBASE);
 
 exit(0);
 
